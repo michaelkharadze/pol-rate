fc221429fe209cf8ea355c878fb6b5a1e6974b4a
unknown <mikhokh123@gmail.com>
2021-09-22 23:17:16 +0400

    HEROKU

--- hackathon_election/settings.py
+++ hackathon_election/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -137,3 +138,5 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'politicalratingsite@gmail.com'
 EMAIL_HOST_PASSWORD = 'Miqaeli123!'
+
+django_heroku.settings(locals())
